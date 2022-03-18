5399fadff05cea10a87c33974be89968722fd761
Vinicius Ricci <viniricci02@gmail.com>
2022-03-18 18:31:22 -0300

    start -> error connect database sql server

---
+++ astrobit-backend/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using astrobit_backend.IdentityAuth;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace astrobit_backend.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
